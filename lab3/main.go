050f15785a146dcce6e0b4219ef9c52c258e4f39
dedr <060806andrew>
2026-03-27 11:10:07 +0200

    feat: initiate statistical Processing Functions

--- lab3/main.go
+++ lab3/main.go
@@ -1,5 +1,7 @@
 package main
 
+import "math"
+
 type LCG struct {
 	a, c, m, current int
 }
@@ -24,6 +26,28 @@ func (lcg *LCG) NextIntn(n int) int {
 
 func (lcg *LCG) NextFloat() float64 {
 	return float64(lcg.Next()) / float64(lcg.m)
+}
+
+func CalculateStats(frequencies map[int]int, k int) (float64, float64, float64) {
+	var expectedValue float64
+	var variance float64
+
+	probabilities := make(map[int]float64)
+
+	for x, freq := range frequencies {
+		p := float64(freq) / float64(k) // P = L / K
+		probabilities[x] = p
+		expectedValue += float64(x) * p // M(X) = Sum(x_i * p_i)
+	}
+
+	for x, p := range probabilities {
+		diff := float64(x) - expectedValue
+		variance += (diff * diff) * p // D(X) = Sum((x_i - M(X))^2 * p_i)
+	}
+
+	stdDev := math.Sqrt(variance) // Sigma = sqrt(D(X))
+
+	return expectedValue, variance, stdDev
 }
 
 func main() {
