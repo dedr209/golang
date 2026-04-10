ce431733b4f35aab114224705552fda7d750cfc9
dedr <060806andrew>
2026-04-10 15:35:21 +0300

    feat: add basic controls to GUI initialization

--- lab4/main.go
+++ lab4/main.go
@@ -10,6 +10,22 @@ func main() {
 
 func initGUI() {
 	window := ui.NewWindow("Lab 4", 800, 600, false)
+
+	// Prepare basic controls now; layout wiring will be added later.
+	entry := ui.NewEntry()
+	combobox := ui.NewCombobox()
+	combobox.Append("Option 1")
+	combobox.Append("Option 2")
+	checkbox := ui.NewCheckbox("Enable option")
+	button := ui.NewButton("Submit")
+	label := ui.NewLabel("Sample label")
+
+	_ = entry
+	_ = combobox
+	_ = checkbox
+	_ = button
+	_ = label
+
 	window.OnClosing(func(*ui.Window) bool {
 		ui.Quit()
 		return true
