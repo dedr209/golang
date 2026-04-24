9096787fb7bc45169286581cdd70ee685ef16c21
dedr <060806andrew>
2026-04-24 16:25:49 +0300

    feat: refactor main function to use constructors for Worker and Company

--- lab5/worker.go
+++ lab5/worker.go
@@ -16,6 +16,38 @@ func NewWorker(name string, year int, month int, workPlace string) Worker {
 	}
 }
 
+func (w Worker) GetName() string {
+	return w.Name
+}
+
+func (w *Worker) SetName(name string) {
+	w.Name = name
+}
+
+func (w Worker) GetYear() int {
+	return w.Year
+}
+
+func (w *Worker) SetYear(year int) {
+	w.Year = year
+}
+
+func (w Worker) GetMonth() int {
+	return w.Month
+}
+
+func (w *Worker) SetMonth(month int) {
+	w.Month = month
+}
+
+func (w Worker) GetWorkPlace() string {
+	return w.WorkPlace
+}
+
+func (w *Worker) SetWorkPlace(workPlace string) {
+	w.WorkPlace = workPlace
+}
+
 type Company struct {
 	Name     string
 	Position string
@@ -28,4 +60,28 @@ func NewCompany(name string, position string, salary int) Company {
 		Position: position,
 		Salary:   salary,
 	}
+}
+
+func (c Company) GetName() string {
+	return c.Name
+}
+
+func (c *Company) SetName(name string) {
+	c.Name = name
+}
+
+func (c Company) GetPosition() string {
+	return c.Position
+}
+
+func (c *Company) SetPosition(position string) {
+	c.Position = position
+}
+
+func (c Company) GetSalary() int {
+	return c.Salary
+}
+
+func (c *Company) SetSalary(salary int) {
+	c.Salary = salary
 }
